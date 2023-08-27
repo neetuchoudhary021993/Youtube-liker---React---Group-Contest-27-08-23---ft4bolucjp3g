e1158b5f7de6be13bdf3e62323ad8077bd807f87
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-08-27 18:11:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,25 @@ import '../styles/App.css';
 import like from '../like.svg';
 
 const App = () => {
+
+  const LikeButton = () => {
   const [count,setCount] = React.useState();
-  
+  const [opacity, setOpacity] = React.useState(0);
+     const toggleLike = () => {
+    setCount(count + 1);
+    setOpacity(opacity + 0.1);
+  };
+    const likeButtonStyle = {
+    backgroundColor: `rgba(255, 0, 0, ${opacity})`,
+    cursor: 'pointer',
+  };
+  }
   return (
     <div id="main">
-      <img id="like-btn-img" style={{fill:'white',width:'100px',backgroundColor:'rgba(255,0,0,0)'}} />
-      <h3>Likes: <span id="like-counter">0</span></h3>
+      <img id="like-btn-img" src="like.svg" alt="Like" tyle={likeButtonStyle}  onClick={toggleLike}/>
+      <h3>Likes: <span id="like-counter">{count}</span></h3>
     </div>
   )
 }
-
 
 export default App;
